84afc7f8cbd9d5d2e67c41ca189201dbcaff52f4
bryan0126 <bryan0126@kookmin.ac.kr>
2025-11-07 14:42:29 +0900

    main함수에서 테스트하는 부분을 제거했습니다.

--- test/aes_test.c
+++ test/aes_test.c
@@ -27,11 +27,7 @@ void test_aes_ctr_256() {
 }
 
 int main() {
-	uint8_t data[2] = {0x11, 0x22};
-	uint8_t pt[2] = {0x23, 0x34};
-	size_t pt_len = 2;
-	SHA2_TYPE a = SHA512_224;
-	sha2_hash(data, pt, pt_len, a);
+
 	printf("SUCCESS!\n");
 	return 0;
 }
